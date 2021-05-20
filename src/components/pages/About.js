762448da5c42ee4642f43c456a19c90ef6807064
SihanCai <cai_s3@denison.edu>
2021-05-20 16:06:13 -0500

    Final Version

---
+++ src/components/pages/About.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="text-center">Book Finder</h1>
+      <p className="lead">
+        Book Finder is a react web app  which utilizes Google Books
+        API that lets you search for books and display information of searching
+        books.
+      </p>
+    </div>
+  );
+};
+
+export default About;
